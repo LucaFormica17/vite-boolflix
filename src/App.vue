df4d9033775c3329c945f3ef03139f020f58cf28
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-13 14:40:57 +0100

    [UPDATE] global state structure MILESTONE 0

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+import axios from 'axios';
+import { store } from './store.js'
+
 export default {
-  
+  components:{
+    AppHeader,
+    AppMain
+  },
+  data() {
+    return {
+      store
+    }
+  },
 }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader />
+    <AppMain />
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use './components/styles/generals.scss';
+
 </style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import {reactive} from "vue";
+
+export const store = reactive ({
+    apiKey: '87cc577ca317d533d9b81bdec95e14fe',
+    apiMovieUrl: 'https://api.themoviedb.org/3/search/movie'
+});
