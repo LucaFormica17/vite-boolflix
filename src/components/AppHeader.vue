a10b28ecf8742aa61c2c18c0192bf16c14dcec28
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-13 15:32:32 +0100

    [UPDATE] added graphic header with title and search-form

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,51 @@ export default {
 </script>
 <template lang="">
     <header>
-
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="nav">
+                        <div class="title">
+                            <h1>Boolflix</h1>
+                        </div>
+                        <div class="search-bar">
+                            <input type="text" class="form-control" placeholder="Film, Serie Tv...">
+                            <button class="btn btn-primary">Cerca</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 <style lang="scss" scoped>
-    
+    @use './styles/generals.scss';
+    @use './styles/partials/variables' as*;
+
+    .nav{
+        display: flex;
+        justify-content: space-between;
+        padding: 10px 0;
+
+        .title{
+
+            h1{
+                color: $red_flix;
+                text-shadow: 2px 2px 5px white;
+            }
+        }
+
+        .search-bar{
+            display: flex;
+            justify-content: end;
+            .form-control{
+                width: 50%;
+
+                &::placeholder{
+                    color: lightgrey;
+                }
+            }
+        }
+    }
+
 </style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import {reactive} from "vue";
 
 export const store = reactive ({
     apiKey: '87cc577ca317d533d9b81bdec95e14fe',
-    apiMovieUrl: 'https://api.themoviedb.org/3/search/movie'
+    apiMovieUrl: 'https://api.themoviedb.org/3/search/movie',
+    moviesList: [],
 });
